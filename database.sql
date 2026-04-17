bf0988858111ccadcd6436dc81375fe9150669c7
Mari kaipeng <kaipengmary01@gmail.com>
2026-04-17 10:06:09 +0530

    Insert sample projects into database
    Add sample project entries to the projects table.

--- database.sql
+++ database.sql
@@ -12,3 +12,8 @@ CREATE TABLE projects (
   description TEXT,
   techstack TEXT[]
 );
+
+INSERT INTO projects (title, description, techstack)
+VALUES
+('Portfolio Website', 'My personal portfolio', ARRAY['HTML','CSS','JS']),
+('Contact API', 'Backend API project', ARRAY['Node.js','PostgreSQL']);
